6394f393f78bf47a17261936f77e4534c0baabef
Ravino <yatakayazvezda@yandex.ru>
2019-01-11 08:05:01 -0500

    Added final fix entry file programm and format conclusion

--- Ivan/search.cpp
+++ Ivan/search.cpp
@@ -7,30 +7,32 @@
 
 
 int main () {
-std::vector <std::vector <int>> matrix = matrixlib::createMatrix ();
+
+  std::cout << "Создание матрицы\n";
+  std::vector <std::vector <int>> matrix = matrixlib::createMatrix ();
 
 
-std::vector <std::vector <int>> resMatrixTransporse = matrixlib::transpose (matrix);
+  std::cout << "Вывод исходной матрицы\n";
+  int resBaseMatrix = matrixlib::printMatrix (matrix);
 
 
-std::cout << "Вывод исходной матрицы\n";
+  std::cout << "\n\nТранспонирование матрицы\n";
+  std::vector <std::vector <int>> resMatrixTransporse = matrixlib::transpose (matrix);
 
 
-int resBaseMatrix = matrixlib::printMatrix (resMatrixTransporse);
+
+  std::cout << "\n\nСортировка столбцов матрицы\n";
+  int resSortRowMatrix = matrixlib::sortRowMatrix (resMatrixTransporse);
 
 
-int resSortRowMatrix = matrixlib::sortRowMatrix (matrix);
+std::cout << "\n\nВыполняется бинарный поиск" << std::endl;
+int resBinarySearchMatrix = matrixlib::binarySearchMatrix (resMatrixTransporse);
 
 
-std::cout << "Выполняется бинарный поиск" << std::endl;
-int resBinarySearchMatrix = matrixlib::binarySearchMatrix (matrix);
-
-
-std::cout << "Выполняется однородный бинарный поиск" << std::endl;
-int resUniformBinarySearchMatrix = matrixlib::uniformBinarySearchMatrix (matrix);
-
-
+std::cout << "\n\nВыполняется однородный бинарный поиск" << std::endl;
+int resUniformBinarySearchMatrix = matrixlib::uniformBinarySearchMatrix (resMatrixTransporse);
 
 
   return 0;
+
 }
