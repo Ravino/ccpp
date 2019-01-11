3ead2be5d0e5bd3b6fb50c22d31e821c735868ca
Ravino <yatakayazvezda@yandex.ru>
2019-01-11 01:04:55 -0500

    Added fix algorithem and format conclusion

--- Ivan/Los/main.cpp
+++ Ivan/Los/main.cpp
@@ -1,3 +1,5 @@
+#include <stdlib.h>
+
 #include <iostream>
 #include <vector>
 
@@ -11,12 +13,12 @@ int printMatrix (std::vector <std::vector <int>> &matrix) {
 
   for (int i = 0; i < matrix. size (); i++) {
 
-    std::cout << std::endl << "Строка: " << i << std::endl;
+    std::cout << std::endl << "Строка: " << i + 1 << std::endl;
 
 
     for (int j = 0; j < matrix [i]. size (); j++) {
 
-      std::cout << j << " ";
+      std::cout << matrix [i] [j] << " ";
 
     }
 
@@ -41,6 +43,16 @@ std::vector <std::vector <int>> createMatrix () {
 
   std::cout << "Введите количество строк матрицы" << std::endl;
   std::cin >> row;
+
+
+  if ((row % 2) > 0) {
+
+    std::cout << "\nЧисло строк должно быть чётным\n\n";
+
+
+    exit (0);
+
+  }
 
 
   std::cout << "Введите количество столбцов матрицы" << std::endl;
@@ -78,28 +90,20 @@ std::vector <std::vector <int>> evenOdd (std::vector <std::vector <int>> matrix)
 
   std::vector <std::vector <int>> newMatrix;
 
-  std::vector <std::vector <int>> even;
-  std::vector <std::vector <int>> odd;
+
+  newMatrix. resize (matrix. size ());
 
 
-  for (int i = 0; i <= matrix. size (); i = i+2) {
+  for (int i = 1; i < matrix. size (); i = i + 2) {
 
-    even. push_back (matrix [i]);
-
-  }
-
-
-  for (int i = 1; i <= matrix. size (); i = i+2) {
-
-    odd. push_back (matrix [i]);
+    newMatrix [i - 1] = matrix [i];
 
   }
 
 
-  for (int i = 0; i < matrix. size (); i++) {
+  for (int i = 1; i < matrix. size (); i = i + 2) {
 
-    newMatrix. push_back (odd [i]);
-    newMatrix. push_back (even [i]);
+    newMatrix [i] = matrix [i - 1];
 
   }
 
@@ -111,15 +115,71 @@ std::vector <std::vector <int>> evenOdd (std::vector <std::vector <int>> matrix)
 
 
 
+std::vector <std::vector <int>> transpose (std::vector <std::vector <int>> &matrix) {
+
+  std::vector <std::vector <int>> newMatrix;
+
+
+  newMatrix. resize (matrix [0]. size ());
+
+
+  for (int i = 0; i < newMatrix. size (); i++) {
+
+    newMatrix [i]. resize (matrix. size ());
+
+  }
+
+
+  for (int i = 0; i < matrix. size (); i++) {
+
+    for (int j = 0; j < matrix [i]. size (); j++) {
+
+      newMatrix [j] [i] = matrix [i] [j];
+
+    }
+
+  }
+
+
+  return newMatrix;;
+
+}
+
+
+
 
 int main () {
 
+  std::cout << "Создание матрицы\n";
   std::vector <std::vector <int>> matrix = createMatrix ();
+
+
+  std::cout << "\n\nИсходная матрица \n";
   printMatrix (matrix);
 
 
-    std::vector <std::vector <int>> evenOddMatrix (matrix);
-    printMatrix (evenOddMatrix);
+  std::cout << "\n\nЗаменяем чётные строки матрицы на нечётные\n";
+  std::vector <std::vector <int>> evenOddMatrix = evenOdd (matrix);
+
+
+  std::cout << "\n\nМатрица с произведённой заменой чётных строк на нечётные\n";
+  printMatrix (evenOddMatrix);
+
+
+  std::cout << "\n\nТранспонирование исходной матрицы\n";
+  std::vector <std::vector <int>> transposeMatrix = transpose (matrix);
+
+
+  std::cout << "\n\nТранспонированная исходная матрица\n";
+  printMatrix (transposeMatrix);
+
+
+  std::cout << "\n\nТранспонирование матрицы с заменёнными чётными строками на нечётные\n";
+  std::vector <std::vector <int>> transposeEvenOddMatrix = transpose (evenOddMatrix);
+
+
+  std::cout << "\n\nТранспонированная матрица с изменёнными чётными строками на нечётные\n";
+  printMatrix (transposeEvenOddMatrix);
 
 
   return 0;
