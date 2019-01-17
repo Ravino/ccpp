ca6b66ca5ecf1125b64c3255eff84901eb8f6196
Ravino <yatakayazvezda@yandex.ru>
2019-01-16 22:21:38 -0500

    Added update algorithm verifyOrder

--- Ivan/matrix.hpp
+++ Ivan/matrix.hpp
@@ -8,19 +8,13 @@ namespace matrixlib {
 
 int verifyOrder (std::vector <int> &vec) {
 
-  int first = vec [0];
-
-
   for (int i = 1; i < vec. size (); i++) {
 
-    if (first > vec [i]) {
+    if (vec [i] <= vec [i - 1]) {
 
       return 0;
 
     }
-
-
-    first = vec [i];
 
   }
 
