f27332534f5879df9387d2cddf50fca85343412f
Ravino <yatakayazvezda@yandex.ru>
2019-01-23 07:17:04 -0500

    Added update codestyle

--- Ivan/src/translit.h
+++ Ivan/src/translit.h
@@ -1,6 +1,7 @@
 #ifndef T2_TRANSLIT_H
-#define T2_TRANSLIT_H
 
-char *translit(const unsigned char *s);
+  #define T2_TRANSLIT_H
+
+  char *translit (const unsigned char *s);
 
 #endif //T2_TRANSLIT_H
