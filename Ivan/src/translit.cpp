c7a2ea11ffdd18fe6a6904c97ab2a1b5e24c4af0
Ravino <yatakayazvezda@yandex.ru>
2019-01-23 07:55:51 -0500

    Added body function translit

--- Ivan/src/translit.cpp
+++ Ivan/src/translit.cpp
@@ -4,33 +4,50 @@
 #include "translit.h"
 
 
-using namespace std;
+#include <cstring>
+#include "translit.h"
 
-typedef unsigned char UCHAR;
 
-const size_t C_SIZE = 4;
-
-//Транслитный русский алфавит на английском
-const UCHAR Angl[][C_SIZE] = {
-        "A", "B", "V", "G", "D", "E", "ZH", "Z", "I", "Y", "K", "L", "M", "N", "O", "P", "R", "S", "T", "U", "F", "H",
-        "C", "CH", "SH", "SCH", "Y", "IY", "", "IE", "YU", "YA"
-};
 
 char *translit(const unsigned char *s) {
-    UCHAR d[] = "МОЛОКО ВКУСНОЕ"; //Строка, которую необходимо транслитнуть
-    for (size_t i = 0, len = sizeof(d); i < len; i++) {
-        UCHAR smv[C_SIZE];
-        if (d[i] == 0xd0 || d[i] == 0xd1) {
-            unsigned short int posit(0);
-            memmove(&posit, s+i, 2 * sizeof(UCHAR));
-            posit = posit - (unsigned short int) 0x90d0;
-            posit = posit % 255;
-            memmove(smv, (void*)Angl[posit], C_SIZE * sizeof(UCHAR));
+    char eng [][4] = {
+            "A", "B", "V", "G", "D", "E", "ZH", "Z", "I", "Y", "K", "L", "M", "N", "O", "P", "R", "S", "T", "U", "F",
+            "H", "TS", "CH", "SH", "SCH", "", "I", "\'", "E", "YU", "YA",
+            "a", "b", "v", "g", "d", "e", "zh", "z", "i", "y", "k", "l", "m", "n", "o", "p", "r", "s", "t", "u", "f",
+            "h", "ts", "ch", "sh", "sch", "", "i", "\'", "e", "yu", "ya"
+    };
+    int len = 0;
+    for (int i = 0; s[i] != '\0'; i++, len++);
+    char *trStr = new char[len * 4];
+    int  j = 0, pos;
+    for (int i = 0; i < len; i++) {
+        if (s[i] == 0xd0 || s[i] == 0xd1){
+            pos = (s[i] << 8) + s[i + 1];
+            if (pos >= 0xd090 && pos < 0xd180){
+                pos -= 0xd090;
+                memcpy(trStr + j, eng[pos], strlen(eng[pos]));
+                j += strlen(eng[pos]);
+            }
+            if (pos >= 0xd180 && pos <=0xd18f){
+                pos = (0xd0bf - 0xd090 + 1) + (pos - 0xd180);
+                memcpy(trStr + j, eng[pos], strlen(eng[pos]));
+                j += strlen(eng[pos]);
+            }
+            if (pos == 0xd081) {
+                memcpy(trStr + j, "YO", strlen("YO"));
+                j += strlen("YO");
+            }
+            else if (pos == 0xd191){
+                memcpy(trStr + j, "yo", strlen("YO"));
+                j += strlen("yo");
+            }
             i++;
-        } else {
-            memmove(smv, d+i, sizeof(UCHAR));
         }
-        cout << smv;
+        else{
+            memcpy(trStr + j, s + i, 1);
+            j++;
+        }
     }
-    return nullptr;
+    trStr[j] = '\0';
+    return trStr;
 }
