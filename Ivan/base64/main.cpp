71b9b13f1c9381f8018f2d8d8b0fc2de45a813e8
Ravino <yatakayazvezda@yandex.ru>
2019-01-24 15:32:19 -0500

    Added normal header file library and test from gtest

---
+++ Ivan/base64/main.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <cstring>
+
+#include "base64.h"
+#include "base64.cpp"
+
+#include <gtest/gtest.h>
+
+
+int main () {
+
+  char * str = new char;
+  char *strEnc = new char;
+  char *strDec = new char;
+
+
+  std::cout << "Введите строку для кодирования \n" << std::endl;
+  std::cin >> str;
+
+
+
+
+
+TEST (base64Tests, encodeTest) {
+
+  ASSERT_STREQ (base64::encode (strTest), base64::encode (str));
+
+}
+
+
+
+TEST (base64Tests, decodeTest) {
+
+  char *strCode = base64::encode (str);
+
+
+  ASSERT_STREQ (base64::decode (strCode), base64::decode (strCode));
+
+}
+
+
+
+TEST (base64Tests, encodeDecodeTest) {
+
+  ASSERT_STREQ (base64::decode (base64::encode (str)), str);
+
+}
+
+
+  std::cout << "Введённая вами строка для кодирования: " << str << std::endl;
+  std::cout << "Производим кодирование \n" << std::endl;
+
+
+  strEnc = base64::encode (str);
+
+
+  std::cout << "Процесс кодирования выполнен. Результат кодирования строки: " << str << std::endl;
+  std::cout << "Имеет вид: " << strEnc << std::endl;
+
+  std::cout << "Строка для декодирования: " << strEnc << std::endl;
+  std::cout << "Производим декодирование \n" << std::endl;
+
+
+  strDec = base64::decode (strEnc);
+
+
+  std::cout << "Процесс декодирования выполнен. Результат декодирования закодированной строки: " << strEnc << std::endl;
+  std::cout << "Имеет вид: " << strDec << std::endl;
+
+
+  return 0;
+}
