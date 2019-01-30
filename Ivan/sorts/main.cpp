328a5bb8d1301c0986a21e83e0cdfb316aedca3b
Ravino <yatakayazvezda@yandex.ru>
2019-01-30 14:27:58 -0500

    Added normal function createVector arm

--- Ivan/sorts/main.cpp
+++ Ivan/sorts/main.cpp
@@ -1,38 +1,43 @@
 #include <iostream>
+#include <vector>
 
 #include "./sorts.cpp"
 
 
 
 
-int *createArr () {
+std::vector <int> createVector () {
+
+  std::vector <int> vec;
+
+  int sizeVec;
+
 
   std::cout << "\n\nВведите размерность массива\n\n";
 
-
-  int sizeArr;
-
-
-  std::cin >> sizeArr;
-
-
-  int arr [sizeArr];
+  std::cin >> sizeVec;
 
 
   std::cout << "\n\nЗаполните массив\n\n";
 
 
-  for (int i = 0;  i < sizeArr;  i++) {
+  for (int i = 0;  i < sizeVec;  i++) {
 
-    std::cout "\nЯчейка " << (i + 1) << " из " << sizeArr << "\n";
+    int elem;
 
 
-    std::cin >> arr [i];
+    std::cout << "\nЯчейка " << (i + 1) << " из " << sizeVec << "\n";
+
+
+    std::cin >> elem;
+
+
+    vec. push_back (elem);
 
   }
 
 
-  return arr;
+  return vec;
 
 }
 
