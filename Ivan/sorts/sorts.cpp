2ddc34292c3038fff684b10e003293fe4c065b94
Ravino <yatakayazvezda@yandex.ru>
2019-01-30 15:16:24 -0500

    Added normal created random array numbers

--- Ivan/sorts/sorts.cpp
+++ Ivan/sorts/sorts.cpp
@@ -122,4 +122,33 @@ int genRand () {
 
 
 
+std::vector <int> createVector () {
+
+  std::vector <int> vec;
+
+  int sizeVec;
+
+
+  std::cout << "\n\nВведите размерность массива\n\n";
+
+  std::cin >> sizeVec;
+
+
+  std::cout << "\n\nЗаполните массив\n\n";
+
+
+  for (int i = 0;  i < sizeVec;  i++) {
+
+    vec. push_back (genRand ());
+
+  }
+
+
+  return vec;
+
+}
+
+
+
+
 }
